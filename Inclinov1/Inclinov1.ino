d2f6ba0200b1fac96762d3e988738bcb3c621936
armisuari <ariefnurmisuari@gmail.com>
2022-11-01 21:07:06 +0700

    dip switch done

--- Inclinov1/Inclinov1.ino
+++ Inclinov1/Inclinov1.ino
@@ -1,13 +1,29 @@
 #include "Arduino.h"
 #include "button.hpp"
 
+Switch dipswitch;
+
 void setup() {
   Serial.begin(115200);  // put your setup code here, to run once:
-  Switch A;
+  dipswitch.begin();
 
-  while (1) {
-    A.read_dip();
-    delay(10);
+  xTaskCreate(
+      dip_switch_task,
+      "dip switch task",
+      5000,
+      NULL,
+      1,
+      NULL);
+
+  while (1) {}
+}
+
+void dip_switch_task(void *parameter)
+{
+  for(;;){
+    dipswitch.read_dip();
+    Serial.println(dipswitch.set_degree());
+    vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
 
--- Inclinov1/button.hpp
+++ Inclinov1/button.hpp
@@ -1,5 +1,4 @@
 // input using switch : dip switch & push button
-
 #pragma once
 
 byte dip_switch[4] = { 2, 4, 18, 19 };
@@ -7,22 +6,40 @@ byte read_switch[4];
 String show_dip;
 
 struct Switch {
-  Switch(){
-    Serial.println(F("\n\ninitializing input pins"));
+  Switch() {}
 
+  void begin() {
+    Serial.println(F("\n\ninitializing input pins"));
     for (byte i = 0; i < 5; i++) {
       pinMode(dip_switch[i], INPUT);
     }
   }
 
-  void read_dip()
-  {
-    for(byte i = 0; i<5; i++)
-    {
+  void read_dip() {
+    for (byte i = 0; i < 5; i++) {
       read_switch[i] = digitalRead(dip_switch[i]);
     }
 
     show_dip = String(read_switch[0]) + String(read_switch[1]) + String(read_switch[2]) + String(read_switch[3]);
     Serial.println(show_dip);
+  }
+
+  byte set_degree() {
+    if (show_dip == "0111") {
+      Serial.println(F("setpoint set to 15 degree"));
+      return 15;
+    } else if (show_dip == "1011") {
+      Serial.println(F("setpoint set to 20 degree"));
+      return 20;
+    } else if (show_dip == "1101") {
+      Serial.println(F("setpoint set to 45 degree"));
+      return 45;
+    } else if (show_dip == "1110") {
+      Serial.println(F("setpoint set to 60 degree"));
+      return 60;
+    } else {
+      Serial.println(F("setpoint not ready"));
+      return 0;
+    }
   }
 };
