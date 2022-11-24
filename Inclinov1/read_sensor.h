c9429a527350499b7feaf018d8dec5d5ef85abd6
IkhwalSyukur <ikhwalsyukur14@gmail.com>
2022-11-24 11:58:27 +0800

    mpu lcd done

--- Inclinov1/read_sensor.h
+++ Inclinov1/read_sensor.h
@@ -3,31 +3,8 @@
 #include <Wire.h>
 #include "RTClib.h"
 #include <MPU6050_tockn.h>
-
 RTC_DS3231 rtc;
 MPU6050 mpu6050(Wire);
-
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-struct RTC {
-  RTC() {}
-
-  void begin() {
-    Wire.begin();
-
-    if (! rtc.begin()) {
-      Serial.println("Couldn't find RTC");
-      while (1);
-    }
-    rtc.adjust(DateTime(__DATE__, __TIME__));
-    //rtc.adjust(DateTime(2022,11,2,11,36,00));
-  }
-
-  String timestamp() {
-    DateTime now = rtc.now();
-    return String(daysOfTheWeek[now.dayOfTheWeek()]) + "," + String(now.hour(), DEC) + ":" + String(now.minute(), DEC) + ":" + String(now.second(), DEC);
-  }
-};
 
 struct Gyro {
   Gyro() {}
@@ -36,11 +13,42 @@ struct Gyro {
     Wire.begin();
     mpu6050.begin();
     mpu6050.calcGyroOffsets(true);
+//    if (! rtc.begin()) {
+//    Serial.println("Couldn't find RTC");
+//    while (1);
+//    }
+//    rtc.adjust(DateTime(__DATE__, __TIME__));
+//    rtc.adjust(DateTime(2022,11,16,21,45,00));
   }
 
   void start(){
     mpu6050.update(); 
   }
+
+//  String daten(){
+//    DateTime now = rtc.now();
+//    return String(daysOfTheWeek[now.dayOfTheWeek()]) + "," + String(now.hour(), DEC) + ":" + String(now.minute(), DEC) + ":" + String(now.second(), DEC);
+//  }
+//
+//  String hari(){
+//    DateTime now = rtc.now();
+//    return String(daysOfTheWeek[now.dayOfTheWeek()]);
+//  }
+//
+//  String jam(){
+//    DateTime now = rtc.now();
+//    return String(now.hour(), DEC);
+//  }
+//
+//  String menit(){
+//    DateTime now = rtc.now();
+//    return String(now.minute(), DEC);
+//  }
+//
+//  String detik(){
+//    DateTime now = rtc.now();
+//    return String(now.second(), DEC);
+//  }
 
   int sudut_x(){
     return mpu6050.getAngleX();
